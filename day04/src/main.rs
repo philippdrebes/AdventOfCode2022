7c55a23eef4370969bc15d4be93cc620a3ebf292
Philipp Drebes <philipp.drebes@gmail.com>
2023-07-30 21:23:46 +0200

    day04: part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -21,6 +21,83 @@ impl Pair {
 
         return first_in_second || second_in_first;
     }
+
+    fn overlapping(&self) -> bool {
+        let overlap1 =
+            self.first.upper >= self.second.lower && self.first.lower <= self.second.lower;
+        let overlap2 =
+            self.second.upper >= self.first.lower && self.second.lower <= self.first.lower;
+        return overlap1 || overlap2;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Bounds;
+    use crate::Pair;
+
+    #[test]
+    fn fully_contained() {
+        let p = Pair {
+            first: Bounds { lower: 5, upper: 7 },
+            second: Bounds { lower: 2, upper: 9 },
+        };
+        assert_eq!(p.one_fully_contains_other(), true);
+    }
+
+    #[test]
+    fn not_fully_contained() {
+        let p = Pair {
+            first: Bounds { lower: 1, upper: 7 },
+            second: Bounds { lower: 5, upper: 9 },
+        };
+        assert_eq!(p.one_fully_contains_other(), false);
+    }
+
+    #[test]
+    fn first_overlapping_second_by_one() {
+        let p = Pair {
+            first: Bounds { lower: 7, upper: 9 },
+            second: Bounds { lower: 5, upper: 7 },
+        };
+        assert_eq!(p.overlapping(), true);
+    }
+
+    #[test]
+    fn second_overlapping_first_by_one() {
+        let p = Pair {
+            first: Bounds { lower: 5, upper: 7 },
+            second: Bounds { lower: 7, upper: 9 },
+        };
+        assert_eq!(p.overlapping(), true);
+    }
+
+    #[test]
+    fn overlap_in_single_section() {
+        let p = Pair {
+            first: Bounds { lower: 6, upper: 6 },
+            second: Bounds { lower: 4, upper: 6 },
+        };
+        assert_eq!(p.overlapping(), true);
+    }
+
+    #[test]
+    fn overlap_in_multiple_sections() {
+        let p = Pair {
+            first: Bounds { lower: 2, upper: 6 },
+            second: Bounds { lower: 4, upper: 8 },
+        };
+        assert_eq!(p.overlapping(), true);
+    }
+
+    #[test]
+    fn no_overlap() {
+        let p = Pair {
+            first: Bounds { lower: 2, upper: 6 },
+            second: Bounds { lower: 60, upper: 82 },
+        };
+        assert_eq!(p.overlapping(), false);
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -40,10 +117,10 @@ fn main() -> io::Result<()> {
             first: parse_bounds(&x[0]),
             second: parse_bounds(&x[1]),
         })
-        .filter(|x| x.one_fully_contains_other())
+        .filter(|x| x.one_fully_contains_other() || x.overlapping())
         .count();
 
-    println!("Found {} contained sections", count);
+    println!("Found {} contained and overlapping sections", count);
 
     Ok(())
 }
